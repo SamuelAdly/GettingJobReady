e21b96f2e773f1c4da854b0cbadef716fc76d0ac
SamuelAdly <90517680+SamuelAdly@users.noreply.github.com>
2025-06-22 01:39:47 -0400

    day 1.1 home page style improvements

--- MY-REACT-PROJECTS/project-1/app/page.js
+++ MY-REACT-PROJECTS/project-1/app/page.js
@@ -2,18 +2,23 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="min-h-screen flex flex-col items-center justify-center">
-      <div>
-        <h1>Welcome to My Project Home Page</h1>
-        <p>Through this project, I am going to relearn Fullstack Development</p>
-        <li><a href="/projects/project-0">HTML+CSS/Tailwind Recap</a>
-        <p>project 0: Relearning HTML+CSS</p></li>
-        <li><a href="/projects/project-1">DevTasks - Task Manager App</a>
-        <p>project 1: Learning React Hooks, Components, Props, etc</p></li>
-        <li><a href="/projects/project-2">StudyHub - Fullstack Course Tracker</a>
-        <p>project 2: Relearning Fullstack Development</p></li>
-        <li><a href="/projects/project-3">DevLink - Developer Portfolio CMS</a>
-        <p>project 3: Relearn backend API`s, forms, file upload, and full CRUD admin</p></li>
+    <main className="min-h-screen flex flex-col items-center justify-center px-2  bg-gray-900 text-white">
+      <div className="border-2 p-6 rounded-2xl max-w-3xl w-full bg-gray-800">
+        <h1 className="text-center text-2xl font-extrabold">Welcome to My Project Home Page</h1>
+        <p className="text-center pb-4 text-md italic">Through these projects I am going to relearn Fullstack Development</p>
+
+        <li><a className="font-bold text-lg hover:underline" href="/projects/project-0">HTML+CSS/Tailwind Recap</a>
+        <p className="italic text-sm">Project 0: Relearning HTML+CSS</p></li>
+
+        <li><a className="font-bold text-lg hover:underline" href="/projects/project-1">DevTasks - Task Manager App</a>
+        <p className="italic text-sm">Project 1: Learning React Hooks, Components, Props, etc</p></li>
+
+        <li><a className="font-bold text-lg hover:underline" href="/projects/project-2">StudyHub - Fullstack Course Tracker</a>
+        <p className="italic text-sm">Project 2: Relearning Fullstack Development</p></li>
+
+        <li><a className="font-bold text-lg hover:underline" href="/projects/project-3">DevLink - Developer Portfolio CMS</a>
+        <p className="italic text-sm">Project 3: Relearn backend API`s, forms, file upload, and full CRUD admin</p></li>
+
       </div>
     </main>
   );
